a7428d9b2ae877f773f569b787a6069cbcb42f92
Rick Armstrong <waitingfortheelectrician@gmail.com>
2025-12-15 04:46:57 +0000

    Fleshed-out unit test for the sensor model jacobian function.

--- test/ekf_localizer_tests.cpp
+++ test/ekf_localizer_tests.cpp
@@ -3,18 +3,21 @@
 #include "gtest/gtest.h"
 #include "ekf_localizer.hpp"
 
+namespace ekf_localizer {
 
 TEST(EKFLocalizerTest, SmokeTest)
 {
-  auto filter = ekf_localizer::Ekf();
+  auto filter = Ekf();
+  Eigen::Vector<double, POSE_DIMS> pose = filter.get_pose();
+  std::cout << pose << std::endl;
 }
 
 TEST(EKFLocalizerTest, MotionModelTest)
 {
-  ekf_localizer::TwistCmd u{1.0, 1.0};
-  ekf_localizer::Pose2D x0{0.0, 0.0, 0.0};
+  TwistCmd u{1.0, 1.0};
+  Pose2D x0{0.0, 0.0, 0.0};
   double delta_t = 1.0;
-  ekf_localizer::Pose2D x1 = g(u, x0, delta_t);
+  Pose2D x1 = g(u, x0, delta_t);
   EXPECT_NEAR(x1(0), sin(1.0), 1e-6);
   EXPECT_NEAR(x1(1), 1.0 - cos(1.0), 1e-6);
   EXPECT_NEAR(x1(2), 1.0, 1e-6);
@@ -22,10 +25,10 @@ TEST(EKFLocalizerTest, MotionModelTest)
 
 TEST(EKFLocalizerTest, FilterTest)
 {
-  ekf_localizer::TwistCmd u{1.0, 1.0};
-  auto filter = ekf_localizer::Ekf();
-  ekf_localizer::EkfState predicted = filter.predict(u, 1.0);
-  ekf_localizer::Pose2D new_pose(predicted.mean(Eigen::seqN(0, 3)));
+  TwistCmd u{1.0, 1.0};
+  auto filter = Ekf();
+  EkfState predicted = filter.predict(u, 1.0);
+  Pose2D new_pose(predicted.mean(Eigen::seqN(0, 3)));
   ASSERT_NEAR(new_pose(0), sin(1.0), 1e-6);
   ASSERT_NEAR(new_pose(1), 1.0 - cos(1.0), 1e-6);
   ASSERT_NEAR(new_pose(2), 1.0, 1e-6);
@@ -40,8 +43,8 @@ TEST(EKFLocalizerTest, FilterTest)
 // the mapping of noise from control space to state space.
 TEST(EKFLocalizerTest, CovPropagationTest)
 {
-  auto filter = ekf_localizer::Ekf();
-  ekf_localizer::TwistCmd u{0.25, 0.0};
+  auto filter = Ekf();
+  TwistCmd u{0.25, 0.0};
   double dt = 0.033;  // 30 Hz.
   double covx;
   const Eigen::Matrix2d control_noise{
@@ -54,7 +57,7 @@ TEST(EKFLocalizerTest, CovPropagationTest)
   // and check that it's not gone insane
   for (auto i = 0; i < 10; ++i)
   {
-    ekf_localizer::EkfState next_state = filter.predict(u, dt, control_noise);
+    EkfState next_state = filter.predict(u, dt, control_noise);
     covx = next_state.covariance(0, 0); // Pose covariance in x.
   }
   EXPECT_LT(covx, 1.0);  // Not blowing-up.
@@ -62,8 +65,22 @@ TEST(EKFLocalizerTest, CovPropagationTest)
 
 TEST(EkfLocalizerTest, SensorJacobianTest)
 {
-  ekf_localizer::Pose2D x_t{0.0, 0.0, 0.0};
+  Pose2D x_t{0.0, 0.0, 0.0};
   Eigen::Vector2d lm{1.0, 0.0};
-  ekf_localizer::SensorJacobian h = ekf_localizer::H_i_t(x_t, lm);
-  double h_0_0 = h(0, 0);
+  uint j = 1;  // Hypothetical landmark index.
+  SensorJacobian h = H_i_t(x_t, lm);
+
+  // h is the low-dimensional Jacobian of the sensor model, w.r.t. the pose and one landmark.
+  // Here, we demonstrate how to 'promote' this to the higher-dimensional version of H_t used in
+  // calculating the Kalman gain.
+  Eigen::Matrix<double, LM_DIMS, STATE_DIMS> H_t;
+  H_t.setZero();
+  H_t.block<LM_DIMS, POSE_DIMS>(0, 0) = h.block<LM_DIMS, POSE_DIMS>(0, 0);
+  H_t.block<LM_DIMS, LM_DIMS>(0, POSE_DIMS + j * LM_DIMS) = h.block<LM_DIMS, LM_DIMS>(0, POSE_DIMS);
+
+  // Landmark component in the right place? Redundant test, but gives a spot for a breakpoint.
+  EXPECT_EQ(H_t(0, POSE_DIMS + j * LM_DIMS), h(0, POSE_DIMS));
+  EXPECT_EQ(H_t(1, POSE_DIMS + j * LM_DIMS + 1), h(1, POSE_DIMS + 1));
 }
+
+}  // namespace ekf_localizer
