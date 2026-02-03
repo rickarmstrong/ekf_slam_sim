dc6d6cd5f3c1ab67adb771a154c3d6142f8d6f65
Rick Armstrong <waitingfortheelectrician@gmail.com>
2026-02-03 06:35:27 +0000

    New RViz file. Add missing file to test builds. slam-11

--- launch/include/tb4_simulation_launch.py
+++ launch/include/tb4_simulation_launch.py
@@ -131,7 +131,7 @@ def generate_launch_description():
 
     declare_rviz_config_file_cmd = DeclareLaunchArgument(
         'rviz_config_file',
-        default_value=os.path.join(bringup_dir, 'rviz', 'nav2_default_view.rviz'),
+        default_value=os.path.join(bringup_dir, 'rviz', 'ekf_slam.rviz'),
         description='Full path to the RVIZ config file to use',
     )
 
